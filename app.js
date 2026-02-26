bb1c4b19a8a66ccb9d8082ae0aca480e0916c585
Amit-1424 <amit1424.work@gmail.com>
2026-02-26 17:53:43 +0530

    test : nothing changed

--- app.js
+++ app.js
@@ -36,12 +36,16 @@ const Listing = require("./models/listing.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
 
+
+
+
+
 app.get("/",(req,res) => {
     res.send("hello i am rooot !")
 });
+
 //index
 app.get("/listings",wrapAsync(  async (req,res) => {
-    
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
 }));
@@ -50,10 +54,11 @@ app.get("/listings",wrapAsync(  async (req,res) => {
 app.get("/listings/new", (req,res) => {
     res.render("listings/new.ejs");
 });
+
 app.post("/listings",wrapAsync(  async (req, res) => {
     if(!req.body.listing){
         throw new expressError(400 ,"send some vbalid data")
-    }   
+    }   // not use
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -72,14 +77,7 @@ app.get("/listings/:id/edit",wrapAsync(  async (req,res) => {
 }));
 app.put("/listings/:id", wrapAsync( async (req,res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(
-        id,
-        req.body.listing,
-        {
-            runValidators: true,
-            new: true
-        }
-    );
+    await Listing.findByIdAndUpdate(id,req.body.listing);
     res.redirect(`/listings/${id}`);
 }));
 //delete
