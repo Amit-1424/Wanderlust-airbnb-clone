f7cf4d0ddce3bf4eb5215db59d2e9dfc90442721
Amit-1424 <amit1424.work@gmail.com>
2026-03-01 14:39:16 +0530

    fixed userSchema-.default

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
     email: {
@@ -10,4 +10,7 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",userSchema);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
