0704090bc9f56fe7bc3d34140220d9784d7c7698
Amit-1424 <amit1424.work@gmail.com>
2026-03-02 11:14:05 +0530

    added post-signup and registerUser

--- models/user.js
+++ models/user.js
@@ -13,4 +13,4 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = User;  
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js")
+const flash = require("connect-flash");
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
 })
+
+router.post("/signup",async (req,res) => {
+    try{
+        const {username,email,password} = req.body;
+        const newUser = new User ({username,email});
+        let registeredUser = await User.register(newUser,password);
+        req.flash("sucess","welcome to wanderlust !");
+        res.redirect("/listings");
+    }catch(e) {
+        req.flash("error",e.message);
+        console.log(e)
+        res.redirect("/signup")
+    }
+
+})
+
+
 
 module.exports = router;
