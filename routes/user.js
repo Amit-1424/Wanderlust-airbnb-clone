fc9506322a42b875f5a8009a78b4a09cd92d61b9
Amit-1424 <amit1424.work@gmail.com>
2026-03-02 11:30:55 +0530

    add get and post for login

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js")
 const flash = require("connect-flash");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport")
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
 })
 
-router.post("/signup",async (req,res) => {
+router.post("/signup", wrapAsync( async (req,res) => {
     try{
         const {username,email,password} = req.body;
         const newUser = new User ({username,email});
@@ -19,9 +21,21 @@ router.post("/signup",async (req,res) => {
         console.log(e)
         res.redirect("/signup")
     }
-
+}));
+router.get("/login",(req,res) => {
+    res.render("users/login.ejs");
 })
 
-
+router.post("/login",
+    passport.authenticate("local",{
+        failureRedirect:"/login",
+        failureFlash:true
+    }),    
+    wrapAsync( async (req,res) => {
+        console.log("hello")
+        let {username} = req.body;
+        req.flash("sucess",` welcome Back ${username} `)
+        res.redirect("/listings");
+}));
 
 module.exports = router;
